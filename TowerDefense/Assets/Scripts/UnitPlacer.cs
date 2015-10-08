71ca43dc53fa1b8ffe748da348a9391eb775bcd2
Jesus Reyes <jesrey4@outlook.com>
2015-10-07 22:32:41 -0500

    Finished UnitPlacer foundation

--- TowerDefense/Assets/Scripts/UnitPlacer.cs
+++ TowerDefense/Assets/Scripts/UnitPlacer.cs
@@ -5,60 +5,73 @@ public class UnitPlacer : MonoBehaviour {
 
     public NodeTemplate[] units;
     Camera viewCamera;
-    public GameObject placeholder;
+    public NodeTemplate placeholder;
     int planeWidth;
     int planeHeight;
-    int unitPos;
     bool canPlace;
+    int currentUnit;
+    bool isPlacing;
 
 	void Start()
     {
         canPlace = false;
         viewCamera = Camera.main;
-        placeholder = Instantiate(placeholder);
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-        Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
-        Plane ground = new Plane(Vector3.up,Vector3.zero);
-        float rayDistance;
-        Vector3 rawPosition;
-
-        if(ground.Raycast(ray, out rayDistance))
+        if(isPlacing)
         {
-            rawPosition = ray.GetPoint(rayDistance);
-            Debug.DrawLine(ray.origin,rawPosition,Color.red);
-            Vector3 newPos = new Vector3(Mathf.Round(rawPosition.x),Mathf.Round(rawPosition.y), Mathf.Round(rawPosition.z));
-            newPos.y += .5f;
-            Debug.DrawLine(ray.origin,newPos, Color.green);
-            placeholder.transform.position = newPos;
-        }
+            Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
+            Plane ground = new Plane(Vector3.up, Vector3.zero);
+            float rayDistance;
+            Vector3 rawPosition;
 
-	    if(canPlace)
-        {
-            if(Input.GetMouseButtonDown(0))
+            if (ground.Raycast(ray, out rayDistance))
             {
-                //PlaceUnit();
+                rawPosition = ray.GetPoint(rayDistance);
+                Debug.DrawLine(ray.origin, rawPosition, Color.red);
+                Vector3 newPos = new Vector3(Mathf.Round(rawPosition.x), Mathf.Round(rawPosition.y), Mathf.Round(rawPosition.z));
+                newPos.y += .5f;
+                Debug.DrawLine(ray.origin, newPos, Color.green);
+                placeholder.transform.position = newPos;
             }
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Place();
+            }
+            
         }
+
 	}
 
-    public void UnitCache(int unit)
+    void StartPlacing(int unitIndex)
     {
-        unitPos = unit;
-        canPlace = true;
+        isPlacing = true;
+        currentUnit = unitIndex;
+        placeholder = Instantiate(units[currentUnit]);
+        placeholder.GetComponent<TurretNode>().enabled = false;
     }
 
-    public void StopPlacing()
+    void Place()
     {
-        canPlace = false;
+        placeholder.GetComponent<TurretNode>().enabled = true;
+        StartPlacing(currentUnit);
     }
 
-    public void PlaceUnit(Vector3 position)
+    void StopPlacing()
     {
+        isPlacing = false;
+    }
 
+    public void ReplaceUnit(int unitIndex)
+    {
+        if(isPlacing && unitIndex != currentUnit)
+        {
+            Destroy(placeholder);
+        }
+        StartPlacing(unitIndex);
     }
 
 }
